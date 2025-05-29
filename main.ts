dbb4914f64868a80a30c26f9d39b5d8ea8a9d9c0
AlexTheManMan <alexey.kozub.al@gmail.com>
2025-05-29 21:06:13 +1000

    made changes

--- main.ts
+++ main.ts
@@ -1,91 +1,83 @@
 function adjustPeriod () {
-    roll = input.rotation(Rotation.Roll)
-    if (roll < 0) {
-        period += -1
-    } else {
-        period += 1
+    angle = input.rotation(Rotation.Roll)
+    if (angle <= -30 && angle >= -90) {
+        radio.sendString("period-")
     }
-    if (period < 5) {
-        period = 5
+    if (angle >= 30 && angle <= 90) {
+        radio.sendString("period+")
     }
-    if (period > 365) {
-        period = 365
-    }
-    led.setBrightness(32)
-    led.plotBarGraph(
-    period - 5,
-    365
-    )
-    radio.sendValue("period", period)
+    kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Big)
+    kitronik_VIEW128x64.show("PERIOD    ", 1, kitronik_VIEW128x64.ShowAlign.Left)
+    kitronik_VIEW128x64.show("" + convertToText(period) + " S         ", 2, kitronik_VIEW128x64.ShowAlign.Left)
 }
 function adjustHue () {
-    roll = input.rotation(Rotation.Roll)
-    if (roll < 0) {
-        HUE += -1
-    } else {
-        HUE += 1
+    angle = input.rotation(Rotation.Roll)
+    if (angle <= -30 && angle >= -90) {
+        radio.sendString("hue-")
     }
-    if (HUE < 1) {
-        HUE = 1
+    if (angle >= 30 && angle <= 90) {
+        radio.sendString("hue+")
     }
-    if (HUE > 360) {
-        HUE = 360
-    }
-    led.setBrightness(32)
-    led.plotBarGraph(
-    HUE,
-    360
-    )
-    radio.sendValue("hue", HUE)
+    kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Big)
+    kitronik_VIEW128x64.show("HUE      ", 1, kitronik_VIEW128x64.ShowAlign.Left)
+    kitronik_VIEW128x64.show("" + convertToText(hue) + "         ", 2, kitronik_VIEW128x64.ShowAlign.Left)
 }
 radio.onReceivedValue(function (name, value) {
     if (name == "bright") {
-        kitronik_VIEW128x64.show("BRIGHTNESS: " + convertToText(Math.round(value)) + "  ", 1)
+        brightness = Math.round(value)
+        if (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
+            kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+            kitronik_VIEW128x64.show("BRIGHTNESS: " + brightness + "          ", 1)
+        }
     }
     if (name == "hue") {
-        kitronik_VIEW128x64.show("HUE: " + convertToText(Math.round(value)) + "  ", 2)
+        hue = Math.round(value)
+        if (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
+            kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+            kitronik_VIEW128x64.show("HUE: " + hue + "             ", 2)
+        }
     }
     if (name == "period") {
-        kitronik_VIEW128x64.show("PERIOD: " + ("" + convertToText(Math.round(value)) + " S  "), 3)
+        period = Math.round(value)
+        if (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
+            kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+            kitronik_VIEW128x64.show("PERIOD: " + period + " S           ", 3)
+        }
     }
     if (name == "temp") {
-        kitronik_VIEW128x64.show("TEMPERATURE: " + ("" + convertToText(Math.round(value)) + " C  "), 4)
+        temp = Math.round(value)
+        if (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
+            kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+            kitronik_VIEW128x64.show("TEMPERATURE: " + temp + " C               ", 4)
+        }
     }
 })
 function adjustBrightness () {
-    roll = input.rotation(Rotation.Roll)
-    if (roll < 0) {
-        brightness += -1
-    } else {
-        brightness += 1
+    angle = input.rotation(Rotation.Roll)
+    if (angle <= -30 && angle >= -90) {
+        radio.sendString("bright-")
     }
-    if (brightness < 3) {
-        brightness = 3
+    if (angle >= 30 && angle <= 90) {
+        radio.sendString("bright+")
     }
-    if (brightness > 255) {
-        brightness = 255
-    }
-    led.setBrightness(brightness)
-    led.plotBarGraph(
-    brightness,
-    255
-    )
-    radio.sendValue("bright", brightness)
+    kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Big)
+    kitronik_VIEW128x64.show("BRIGHTNESS", 1, kitronik_VIEW128x64.ShowAlign.Left)
+    kitronik_VIEW128x64.show("" + convertToText(brightness) + "         ", 2, kitronik_VIEW128x64.ShowAlign.Left)
 }
+let temp = 0
 let period = 0
-let HUE = 0
+let hue = 0
 let brightness = 0
-let roll = 0
-radio.setGroup(1)
-roll = 0
 let angle = 0
+radio.setGroup(1)
+let roll = 0
+angle = 0
 brightness = 16
-HUE = 180
+hue = 180
 let height_brightness = 128
 period = 128
+temp = 0
 led.setBrightness(brightness)
-basic.showString("A B AB")
-kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
         adjustHue()
