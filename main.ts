000d337d8a4338987164d887f1103445d4cf18c3
AlexTheManMan <alexey.kozub.al@gmail.com>
2025-05-30 10:30:20 +1000

    Introduce sleep mode

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+function printHints () {
+    if (sleepMode == 0) {
+        kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
+        kitronik_VIEW128x64.clearLine(5)
+        kitronik_VIEW128x64.clearLine(6)
+        kitronik_VIEW128x64.show("A:HUE           AB:PERIOD", 7)
+        kitronik_VIEW128x64.show("B:BRIGHTNESS     LOGO:OFF", 8)
+    } else {
+        kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Big)
+        kitronik_VIEW128x64.clearLine(3)
+        kitronik_VIEW128x64.clearLine(4)
+        kitronik_VIEW128x64.show("PRESS LOGO TO", 3, kitronik_VIEW128x64.ShowAlign.Centre)
+        kitronik_VIEW128x64.show("TURN ON", 4, kitronik_VIEW128x64.ShowAlign.Centre)
+    }
+}
 function adjustPeriod () {
     angle = input.rotation(Rotation.Roll)
     if (angle <= -30 && angle >= -90) {
@@ -9,6 +24,14 @@ function adjustPeriod () {
     kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Big)
     kitronik_VIEW128x64.show("PERIOD    ", 1, kitronik_VIEW128x64.ShowAlign.Left)
     kitronik_VIEW128x64.show("" + toMinutesAndSeconds(period) + "           ", 2, kitronik_VIEW128x64.ShowAlign.Left)
+}
+function powerToggle () {
+    if (sleepMode == 1) {
+        radio.sendString("Sleep")
+    }
+    if (sleepMode == 0) {
+        radio.sendString("Wake")
+    }
 }
 function toMinutesAndSeconds (totalSeconds: number) {
     minutes = convertToText(Math.floor(totalSeconds / 60))
@@ -31,6 +54,12 @@ function adjustHue () {
     kitronik_VIEW128x64.show("" + convertToText(hue) + "         ", 2, kitronik_VIEW128x64.ShowAlign.Left)
 }
 radio.onReceivedValue(function (name, value) {
+    if (name == "sleep") {
+        if (sleepMode != value) {
+            sleepMode = value
+            printHints()
+        }
+    }
     if (name == "bright") {
         brightness = Math.round(value)
         if (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
@@ -79,14 +108,17 @@ let period = 0
 let hue = 0
 let brightness = 0
 let angle = 0
+let sleepMode = 0
 radio.setGroup(1)
+sleepMode = 1
 angle = 0
 brightness = 16
 hue = 180
 let height_brightness = 128
 period = 128
 temp = 0
-kitronik_VIEW128x64.show("A:HUE  B:BRIGHT AB:PERIOD", 8)
+kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
+printHints()
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
         adjustHue()
@@ -96,5 +128,8 @@ basic.forever(function () {
     }
     if (input.buttonIsPressed(Button.AB)) {
         adjustPeriod()
+    }
+    if (input.logoIsPressed()) {
+        powerToggle()
     }
 })
